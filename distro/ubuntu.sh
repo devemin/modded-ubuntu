857c34b8cd9d863c243e8278c775f8a841588b09
devemin <52738228+devemin@users.noreply.github.com>
2023-10-12 21:33:51 +0900

    Update ubuntu.sh

--- distro/ubuntu.sh
+++ distro/ubuntu.sh
@@ -3,8 +3,8 @@
 # If you want customize installation, please make a copy.
 DISTRO_NAME="Ubuntu (focal)"
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v2.2.0/ubuntu-aarch64-pd-v2.2.0.tar.xz"
-TARBALL_SHA256['aarch64']="486de37668963c1b6a0d131e33b91486be8eb1919b0813ad03726885753feba6"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v2.2.0/ubuntu-arm-pd-v2.2.0.tar.xz"
+TARBALL_SHA256['arm']="b29f6d89c5d19056b297125a1a8222f641579bd2c70d2174ac91080100ec634a"
 
 distro_setup() {
 	# Don't update gvfs-daemons and udisks2
